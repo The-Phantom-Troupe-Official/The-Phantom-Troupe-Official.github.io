4c52cc499628f41478740c67e0a563eb9ecd88ce
NathanielAddo <stevenelly100@gmail.com>
2024-09-02 22:47:21 +0000

    chore: Update asset paths in build files

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Sidebar from './sideBar';
+import Sidebar from './Sidebar';
 import ProductGrid from './ProductGrid';
 
 function ProductPage() {
