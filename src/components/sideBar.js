033e132ce6a80ab8f390a4d565caa8a2927f8225
NathanielAddo <stevenelly100@gmail.com>
2024-09-11 20:13:48 +0000

    Refactor Sidebar component to add 'All' category and highlight selected category

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -1,19 +1,23 @@
 import React from 'react';
 
-function Sidebar({ onCategorySelect }) {
-  const categories = ['System Units', 'Desktop PCs', 'Workstations', 'Gaming PCs', 'Mini PCs',
-    'Monitors', 'Computer Accessories', 'All-in-One Computers', 'Laptops',
-    'Macs', 'Customized PCs'];
+function Sidebar({ onCategorySelect, selectedCategory }) {
+  const categories = ['All', 'System Units', 'Desktop PCs', 'Workstations', 'Gaming PCs', 
+    'Mini PCs', 'Monitors', 'Computer Accessories', 'All-in-One Computers', 
+    'Laptops', 'Macs', 'Customized PCs'];
 
   const handleCategoryClick = (category) => {
-    onCategorySelect(category);
+    onCategorySelect(category); // Pass the clicked category to the parent
   };
 
   return (
     <aside className="w-64">
       <ul>
         {categories.map((category, index) => (
-          <li key={index} onClick={() => handleCategoryClick(category)} className="cursor-pointer p-4 hover:bg-gray-200">
+          <li
+            key={index}
+            onClick={() => handleCategoryClick(category)}
+            className={`cursor-pointer p-4 hover:bg-gray-200 ${selectedCategory === category ? 'bg-gray-300' : ''}`}
+          >
             {category}
           </li>
         ))}
